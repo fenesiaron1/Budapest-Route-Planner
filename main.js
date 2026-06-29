c04bb27f5a7f514621a6e87c64151ab73b68d91b
Fenesi Áron <fenesiaron1@gmail.com>
2026-06-29 15:59:17 +0300

    Added marker and route layer

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 import './style.css';
 import {Map, View} from 'ol';
+import { fromLonLat } from 'ol/proj';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import { Icon, Style } from 'ol/style';
+
+import LineString from 'ol/geom/LineString.js';
+import Stroke from 'ol/style/Stroke.js';
+
 
 const map = new Map({
   target: 'map',
@@ -11,7 +22,27 @@ const map = new Map({
     })
   ],
   view: new View({
-    center: [0, 0],
-    zoom: 2
+    center: fromLonLat([19.040235, 47.497912]),
+    zoom: 12
   })
 });
+
+const markerLayer = new VectorLayer({
+    source: new VectorSource({
+        features: []
+    })
+});
+
+const routeLayer = new VectorLayer({
+    source: new VectorSource({
+        features: []
+    })
+});
+
+map.addLayer(markerLayer);
+map.addLayer(routeLayer);
+
+
+
+
+
